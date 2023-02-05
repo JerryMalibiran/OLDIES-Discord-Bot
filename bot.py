79f9720492899d6055e3d965b298f16a2814758f
JerryMalibiran <jerry.u.malibiran@gmail.com>
2023-02-05 10:07:18 +0800

    Convert commands to slash commands

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ def run(token, ai):
 
     @bot.event
     async def on_ready():
+        await bot.tree.sync()
         print(f'Logged in {bot.user} ({bot.user.id})')
 
     @bot.command()
@@ -17,12 +18,19 @@ def run(token, ai):
         response = ai.completion(prompt)
         await ctx.reply(response)
 
-    @bot.command()
-    async def image(ctx, *, prompt):
+    @bot.tree.command(name="gpt", description="Talk to an AI. Ask Anything!")
+    async def gpt(interaction: discord.Interaction, prompt: str):
+        response = ai.completion(prompt)
+        await interaction.response.send_message(response)
+
+    @bot.tree.command(name="image", description="Create an AI generated image from a text prompt. How about a cat?")
+    async def image(interaction: discord.Interaction, prompt: str):
+        await interaction.response.defer()
+
         response = ai.generation(prompt)
         embed = discord.Embed(title="Generated Image",
                               description=prompt, url=response)
         embed.set_image(url=response)
-        await ctx.reply(embed=embed)
+        await interaction.followup.send(embed=embed)
 
     bot.run(token)
